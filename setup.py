1274633158dcdc3c68f7636641b19fa71da15fb4
Dineshkarthik <dineshkarthik.r@gmail.com>
2020-11-25 22:58:14 +0100

    add: setup.py for versioning

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="wiki_contribution",
+    packages=[
+        "wiki_contribution",
+    ],
+    include_package_data=True,
+    install_requires=[],
+    version="0.0.1",
+)
